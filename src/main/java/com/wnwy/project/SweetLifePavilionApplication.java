811b8d1967dba67cc6a4caf5d762bb710790e6b0
wang18575744554 <wang18575744554@163.com>
2019-09-30 15:22:38 +0800

    application

--- src/main/java/com/wnwy/project/SweetLifePavilionApplication.java
+++ src/main/java/com/wnwy/project/SweetLifePavilionApplication.java
@@ -1,8 +1,10 @@
 package com.wnwy.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.wnwy.project.mapper")
 @SpringBootApplication
 public class SweetLifePavilionApplication {
 
