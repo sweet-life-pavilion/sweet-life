302b2102e8b99c542fb194447a991fe7f07048b5
wang18575744554 <wang18575744554@163.com>
2019-09-30 14:27:46 +0800

    增加了type实体类，mapper层，xml层

---
+++ src/main/java/com/wnwy/project/entities/Type.java
@@ -0,0 +1,27 @@
+package com.wnwy.project.entities;
+
+import java.io.Serializable;
+
+public class Type implements Serializable {
+    private Integer typeId;
+
+    private String typeName;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName == null ? null : typeName.trim();
+    }
+}
---
+++ src/main/java/com/wnwy/project/mapper/TypeMapper.java
@@ -0,0 +1,17 @@
+package com.wnwy.project.mapper;
+
+import com.wnwy.project.entities.Type;
+
+public interface TypeMapper {
+    int deleteByPrimaryKey(Integer typeId);
+
+    int insert(Type record);
+
+    int insertSelective(Type record);
+
+    Type selectByPrimaryKey(Integer typeId);
+
+    int updateByPrimaryKeySelective(Type record);
+
+    int updateByPrimaryKey(Type record);
+}
